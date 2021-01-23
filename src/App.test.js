912b8f71f110aa01361dd481ae00dfb10c79cf27
michaeljymsgutierrez <leeez711@gmail.com>
2021-01-23 21:34:44 +0800

    Added test for kml

--- src/App.test.js
+++ src/App.test.js
@@ -1,29 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("KML Extractor test", () => {
   const root = document.createElement("div");
 
   it("Renders without crashing", () => {
-    ReactDOM.render(<App />, root);
-    ReactDOM.unmountComponentAtNode(root);
+    const { container } = render(<App />, root);
+    expect(container).toBeTruthy();
   });
 
   it("Add google api key button renders correctly", () => {
-    ReactDOM.render(<App />, root);
-    expect(root.querySelectorAll("button:first-child")[0].textContent).toBe(
+    const { getByText } = render(<App />, root);
+    expect(getByText("Add Google API Key").textContent).toBe(
       "Add Google API Key"
     );
-    ReactDOM.unmountComponentAtNode(root);
   });
 
   it("Verify coordinates button renders correctly", () => {
-    ReactDOM.render(<App />, root);
-    expect(root.querySelectorAll("button:last-child")[0].textContent).toBe(
+    const { getByText } = render(<App />, root);
+    expect(getByText("Verify Coordinates").textContent).toBe(
       "Verify Coordinates"
     );
-    ReactDOM.unmountComponentAtNode(root);
   });
 });
